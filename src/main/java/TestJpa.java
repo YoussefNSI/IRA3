1dd88a111e87765bef2b4f9842a3ab74d16387e5
Youssef <90571117+YoussefNSI@users.noreply.github.com>
2026-02-19 13:41:48 +0100

    init

---
+++ src/main/java/TestJpa.java
@@ -0,0 +1,22 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class TestJpa {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu-essai");
+        EntityManager em = emf.createEntityManager();
+
+        if (em.isOpen()) {
+            System.out.println("Connexion à la base de données réussie !");
+        } else {
+            System.out.println("Échec de la connexion.");
+        }
+
+
+
+        em.close();
+        emf.close();
+    }
+}
