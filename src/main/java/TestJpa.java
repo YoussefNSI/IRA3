061007ebbdba4e7b242f612bfcf421edfc8ba042
Youssef <90571117+YoussefNSI@users.noreply.github.com>
2026-02-19 13:43:15 +0100

    init2

--- src/main/java/TestJpa.java
+++ src/main/java/TestJpa.java
@@ -3,7 +3,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class TestJpa {
-    public static void main(String[] args) {
+    static void main() {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu-essai");
         EntityManager em = emf.createEntityManager();
